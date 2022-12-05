3e2efd8de7421539ccc3a8822621e67c313b57e7
santiagoprueba <santiago@gmail.com>
2022-12-04 22:26:34 -0500

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Register from "../src/components/Authentication/Register";
 import LandingPage from "../src/components/LandingPage/LandingPage";
 import Login from "../src/components/Authentication/Login";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
         <Route path="/landing-page" element={<LandingPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
