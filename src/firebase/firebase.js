a5e51748183720075fe5e4672794dd408e39c3ca
santiagoprueba <santiago@gmail.com>
2022-12-04 22:31:20 -0500

    config

--- src/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyCuNY4czJ_CrgWv_-fsMERviJiFMws3JxY",
-  authDomain: "prueba-tecnica-v1.firebaseapp.com",
-  projectId: "prueba-tecnica-v1",
-  storageBucket: "prueba-tecnica-v1.appspot.com",
-  messagingSenderId: "237362297502",
-  appId: "1:237362297502:web:14e284c8fcf48f3455998f",
-  measurementId: "G-79GSRMTEX0",
-};
